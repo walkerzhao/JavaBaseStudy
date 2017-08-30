4cd3955a94e3a4207242ad9a50af470e965f9ce1
andy <416548283@qq.com>
2017-08-30 23:35:18 +0800

    add jstack/jstat/jmap的测试程序

---
+++ JavaBase/src/main/java/com/tencent/memory/MemToolTest.java
@@ -0,0 +1,17 @@
+package com.tencent.memory;
+
+/**
+ * java的性能分析工具测试
+ * @author andy
+ *
+ */
+public class MemToolTest {
+	public static void main(String[] args) throws InterruptedException {
+		int i = 0;
+		while(true) {
+			System.out.println("hello,world"+(i++));
+			Thread.sleep(1000);
+		}
+	}
+
+}
