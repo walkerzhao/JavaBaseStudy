4ea722ea8d43a124df9b78ab44d18419a6df3bee
andy <416548283@qq.com>
2017-12-27 00:26:28 +0800

    add sleep and wait diff demo

--- src/main/java/com/tencent/java/multithread/ThreadSleepAndWaitDiff.java
+++ src/main/java/com/tencent/java/multithread/ThreadSleepAndWaitDiff.java
@@ -6,8 +6,48 @@ package com.tencent.java.multithread;
  *
  */
 public class ThreadSleepAndWaitDiff {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		ThreadB threadb = new ThreadB();
+		Thread thread = new Thread(threadb);
+		thread.start();
 		
+		Thread.sleep(300);
+		synchronized (threadb) {
+			System.out.println("get threadb monitor..");
+			try {
+				Thread.sleep(400);
+				
+				threadb.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("back.");
+		}
 	}
 
+}
+
+
+class ThreadB implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// TODO Auto-generated method stub
+		synchronized (this) {
+			System.out.println("thread:"+Thread.currentThread().getName() + " running");
+			for(int i =0; i<100; i++) {
+				System.out.println("i:"+i);
+			}
+			notify();
+		}
+		
+	}
+	
 }
