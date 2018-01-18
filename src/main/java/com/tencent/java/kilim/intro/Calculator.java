51964beb360573f3a3c04ccf4e509733632ae02f
andy <416548283@qq.com>
2018-01-19 01:28:22 +0800

    add mailbox putn test

--- src/main/java/com/tencent/java/kilim/intro/Calculator.java
+++ src/main/java/com/tencent/java/kilim/intro/Calculator.java
@@ -4,26 +4,25 @@ import java.math.RoundingMode;
 
 import kilim.*;
 
-public class Calculator extends Task{
-	 
-	 private Mailbox<Calculation> mailbox;
-	 
-	 public Calculator(Mailbox<Calculation> mailbox) {
-	  super();
-	  this.mailbox = mailbox;
-	 }
-	 
-	 @Override
-	 public void execute() throws Pausable, Exception {
-	  while (true) {            
-	   Calculation calc = mailbox.get(); // blocks
-	   if (calc.getAnswer() == null) {
-	    calc.setAnswer(calc.getDividend().divide(calc.getDivisor(), 8, 
-	      RoundingMode.HALF_UP));               
-	    System.out.println("Calculator determined answer");
-	    mailbox.putnb(calc);
-	   }
-	   Task.sleep(1000);
-	  }
-	 }
+public class Calculator extends Task {
+
+	private Mailbox<Calculation> mailbox;
+
+	public Calculator(Mailbox<Calculation> mailbox) {
+		super();
+		this.mailbox = mailbox;
 	}
+
+	@Override
+	public void execute() throws Pausable, Exception {
+		while (true) {
+			Calculation calc = mailbox.get(); // blocks
+			if (calc.getAnswer() == null) {
+				calc.setAnswer(calc.getDividend().divide(calc.getDivisor(), 8, RoundingMode.HALF_UP));
+				System.out.println("Calculator determined answer");
+				mailbox.putnb(calc);
+			}
+			Task.sleep(1000);
+		}
+	}
+}
--- src/main/java/com/tencent/java/kilim/intro/DeferredDivision.java
+++ src/main/java/com/tencent/java/kilim/intro/DeferredDivision.java
@@ -10,28 +10,27 @@ import kilim.Pausable;
 import kilim.Task;
 
 public class DeferredDivision extends Task {
-	 
-	 private Mailbox<Calculation> mailbox;
-	 
-	 public DeferredDivision(Mailbox<Calculation> mailbox) {
-	  super();
-	  this.mailbox = mailbox;
-	 }
-	 
-	 @Override
-	 public void execute() throws Pausable, Exception {
-	  Random numberGenerator = new Random(new Date().getTime());
-	  MathContext context = new MathContext(8);
-	  while (true) {
-	   System.out.println("I need to know the answer of something");
-	   mailbox.putnb(new Calculation(
-	     new BigDecimal(numberGenerator.nextDouble(), context), 
-	     new BigDecimal(numberGenerator.nextDouble(), context)));
-	   Task.sleep(1000);
-	   Calculation answer = mailbox.getnb(); // no block
-	   if (answer != null && answer.getAnswer() != null) {
-	    System.out.println("Answer is: " + answer.printAnswer());
-	   }
-	  }
-	 }
+
+	private Mailbox<Calculation> mailbox;
+
+	public DeferredDivision(Mailbox<Calculation> mailbox) {
+		super();
+		this.mailbox = mailbox;
 	}
+
+	@Override
+	public void execute() throws Pausable, Exception {
+		Random numberGenerator = new Random(new Date().getTime());
+		MathContext context = new MathContext(8);
+		while (true) {
+			System.out.println("I need to know the answer of something");
+			mailbox.putnb(new Calculation(new BigDecimal(numberGenerator.nextDouble(), context),
+					new BigDecimal(numberGenerator.nextDouble(), context)));
+			Task.sleep(1000);
+			Calculation answer = mailbox.getnb(); // no block
+			if (answer != null && answer.getAnswer() != null) {
+				System.out.println("Answer is: " + answer.printAnswer());
+			}
+		}
+	}
+}
