1c78152cc4d2ede51a4f9b6956574cd9f16d320f
ewanzhao(赵雨) <ewanzhao@tencent.com>
2018-02-28 21:19:26 +0800

    add hashmap test

--- src/main/java/com/tencent/java/kilim/XuanzongKilimBugFix.java
+++ src/main/java/com/tencent/java/kilim/XuanzongKilimBugFix.java
@@ -12,15 +12,16 @@ import kilim.Mailbox;
 public class XuanzongKilimBugFix {
 	public static void main(String[] args) throws InterruptedException {
 		int initailSize = 5;
-		int maxSize = 3000_0000;
+//		int maxSize = 3000_0000;
+		int maxSize = 5;
 		Mailbox<Integer> mailbox = new Mailbox<>(initailSize, maxSize); 
 		Producer producer = new Producer(mailbox, maxSize); 
 		new Thread(producer).start();
 		
 		Thread.sleep(1000);
 		
-//		Consumer consumer = new Consumer(mailbox, maxSize);
-//		new Thread(consumer).start();
+		Consumer consumer = new Consumer(mailbox, maxSize);
+		new Thread(consumer).start();
 		
 	}
 	
@@ -45,8 +46,7 @@ class Producer implements Runnable {
 
 	@Override
 	public void run() {
-		testMailBoxPutbMsg();
-		
+		testMailBoxPutbMsg();		
 	}
 	
 	private  void testMailBoxPutbMsg() {
@@ -54,24 +54,22 @@ class Producer implements Runnable {
 		
 		long start = System.currentTimeMillis();
 		for(int i=0; i< (maxSize+1);i++) {
-			synchronized (mailbox) {
-				mailbox.putb(i);
-				if(mailbox.size()==maxSize) {
-					long end = System.currentTimeMillis();
-					System.out.println("pub msg suc:"+i +" mailbox size:" + mailbox.size() );
-					System.out.println("cost:" + (end-start));
-				}
-//				System.out.print("mailbox data: ");
-//				for(int j=0; j< ((Object[])mailbox.msgs).length; j++) {
-//					System.out.print(((Object[])mailbox.msgs)[j] +" ");
-//				}
-
-//				System.out.println();
-				
-			}
+			
+			mailbox.putb(i);
+			System.out.println("put msg suc:"+i +" size:"+mailbox.size());
+//			if(mailbox.size()==maxSize) {
+//				long end = System.currentTimeMillis();
+//				System.out.println("pub msg suc:"+i +" mailbox size:" + mailbox.size() );
+//				System.out.println("cost:" + (end-start));				
+//			}
+			
+//			System.out.print("mailbox data: ");
+//			for(int j=0; j< ((Object[])mailbox.msgs).length; j++) {
+//				System.out.print(((Object[])mailbox.msgs)[j] +" ");
+//			}
+//			System.out.println(" mailbox.size:"+mailbox.size());
 			
 		}
-		System.out.println("final size:"+mailbox.size());
 		
 	}
 	
@@ -93,19 +91,20 @@ class Consumer implements Runnable {
 		System.out.println("consume msg from mailbox");
 		int cnt = 0;
 		while(true) {
-
 			Integer integer = mailbox.getb();
 			System.out.println("get msg:"+integer);
 			
 			try {
-				Thread.sleep(1000);    //隔1s消费一个--for 生产者足够时间put数据
+				Thread.sleep(10);    //隔1s消费一个--for 生产者足够时间put数据
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			cnt++;
-			if(cnt == 11)
+			if(cnt == maxSize+1) {
+				System.out.println("maxSize."+maxSize);
 				break;
+			}
 		}
 		// TODO Auto-generated method stub
 		
