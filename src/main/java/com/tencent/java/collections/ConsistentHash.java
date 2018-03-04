701fbf113cd0d8e39848859268f7c24e178874c9
andy <416548283@qq.com>
2018-03-04 18:24:48 +0800

    add 一致性哈市算法

---
+++ src/main/java/com/tencent/java/collections/ConsistentHash.java
@@ -0,0 +1,11 @@
+package com.tencent.java.collections;
+
+/**
+ * 一致性哈希的研究：https://www.cnblogs.com/xrq730/p/5186728.html
+ * 一致性哈希的深入研究。
+ * @author andy
+ *
+ */
+public class ConsistentHash {
+
+}
